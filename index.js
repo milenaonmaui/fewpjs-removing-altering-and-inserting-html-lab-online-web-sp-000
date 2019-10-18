94d8423dff827743f2c2af0c177a8dc4fd61a0b4
Milena Kari <milenakari@yahoo.com>
2019-10-18 21:09:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let element = document.querySelector("main#main");
 element.remove();
 let newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
-document.body.appendChild(newHeader)
+document.body.appendChild(newHeader);
+newHeader.innerHTML=
