6c23cffc319f6fc0f8b43346ba0c22adb065206f
Milena Kari <milenakari@yahoo.com>
2019-10-18 21:01:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 let element = document.querySelector("main#main");
 element.remove();
-let newHeader = document.querySelector()
+let newHeader = document.querySelector("h1#victory")
