dfa2d1168398d0a651586928343613e16ef5a001
Milena Kari <milenakari@yahoo.com>
2019-10-18 21:10:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let element = document.querySelector("main#main");
+element.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML="YOUR-NAME is the champion"
